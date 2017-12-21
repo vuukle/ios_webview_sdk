6390d162d3fb17dfa6c8bd389316c85edd2f52bc
Klymenko Fedir <fkklymenko@gmail.com>
2017-12-21 14:30:02 +0200

    Initial Commit

---
+++ VukkleExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VukkleExample
+//
+//  Created by MAC_7 on 12/21/17.
+//  Copyright © 2017 MAC_7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
