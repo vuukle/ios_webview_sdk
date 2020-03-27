2051d67c09d77ef05cb4dc0ee3f128ee5121e486
Nazar <nazar.suhovych@gmail.com>
2020-03-27 14:57:36 +0200

    Disable 3D touch links preview and add redirection for Privacy & Vuukle

--- Vuukle/Vuukle.framework/Headers/Vuukle.h
+++ Vuukle/Vuukle.framework/Headers/Vuukle.h
@@ -16,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char VuukleVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Vuukle/PublicHeader.h>
 
-
